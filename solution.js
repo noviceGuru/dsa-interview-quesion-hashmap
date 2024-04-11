d003a4aeebf70d5eeefbb317501234db5ec6da35
noviceGuru <amin.arabi@gmail.com>
2024-04-11 10:19:57 -0400

    added documentation and two examples

--- solution.js
+++ solution.js
@@ -1,3 +1,55 @@
+/* THIS IS A SOLUTION TO THE FOLLOWING QUESTION:
+
+
+You will be given a large set of data with the following JSON format in `data.json`
+```
+{
+  "idina_response":{
+    "sources":[
+      {
+        "url":"example.com",
+        "spam_score": 1,
+        "matching_target_indexes":[
+          0
+        ],
+        "matching_source_urls":[
+          {
+            "url":"example.com/news/current/",
+            "page_authority":44
+          }
+        ],
+        "domain_authority":95
+      },
+      // ...
+    ]
+  }
+}
+```
+From this data we would like you to write code that produces two lists. The first will
+be the list of all the URLs that have a duplicate `spam_score`,
+and the second will be a list of all the URLs with a duplicate `domain_authority`. */
+
+
+/* THE ANSWER:
+
+I used two hash maps to track spam_score and domain_authorities respectively. I also created two
+Sets to save the duplicate spam scores and domain authorities, since it will be a unique array.
+
+Then, I iterated over the data json, first checking if the map already contains the spam_score (or in
+the other case domain_authorities). If it already contained it, I add the url to the set, and also the url that
+is already present in the hash map.
+
+Using has maps and sets (which both use built in hash functions), searching time would decrease to O(1),
+and the operation would be easily scalable.
+
+In the end, I printed the outcome of the two examples provided in the question, and also tried to add two corner
+cases as examples. Running the file, you'll see the outcomes printed.
+
+My apologies for not separating the documentation file and the data file, since I tried to follow
+what the question suggested, to put all in a text file, so it would be also readable easily.
+
+*/
+
 const exampleOne = {
     "idina_response": {
         "sources": [
@@ -56,9 +108,79 @@ const exampleTwo = {
       ]
     }
   }
+  
+  const exampleThree = {
+      "idina_response":{
+          "sources":[
+              {
+          "url":"example.com",
+          "spam_score": 1,
+          "matching_target_indexes":[
+              0
+            ],
+            "matching_source_urls":[
+                {
+              "url":"example.com/news/current/",
+              "page_authority":44
+            }
+        ],
+        "domain_authority":95
+    },
+    {
+        "url":"example.com",
+        "spam_score": 1,
+        "matching_target_indexes":[
+            0
+          ],
+          "matching_source_urls":[
+              {
+              "url":"example.com/news/current/",
+              "page_authority":44
+            }
+        ],
+        "domain_authority":95
+    },
+    {
+        "url":"example.com",
+        "spam_score": 1,
+        "matching_target_indexes":[
+            0
+        ],
+        "matching_source_urls":[
+            {
+                "url":"example.com/news/current/",
+                "page_authority":44
+            }
+        ],
+        "domain_authority":95
+    },
+    {
+        "url":"example.com",
+        "spam_score": 1,
+        "matching_target_indexes":[
+            0
+        ],
+        "matching_source_urls":[
+            {
+                "url":"example.com/news/current/",
+                "page_authority":44
+            }
+        ],
+        "domain_authority":95
+    },
+    
+]
+}
+}
+
+const exampleFour = {
+    "idina_response":{
+      "sources":[]
+    }
+  }
 
 
-const getDuplicates = (data) => {
+  const getDuplicates = (data) => {
     const spamScoresMap = new Map()
     const domainAuthoritiesMap = new Map()
 
@@ -90,5 +212,9 @@ const getDuplicates = (data) => {
     return duplicates
 }
 
+
+// Test cases
 console.log('Example One: ',getDuplicates(exampleOne))
 console.log('Example Two: ',getDuplicates(exampleTwo))
+console.log('Example Three: ',getDuplicates(exampleThree))
+console.log('Example Four: ',getDuplicates(exampleFour))
