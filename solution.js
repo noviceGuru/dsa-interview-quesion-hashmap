8ab20bb960783612aa53294303f22c1e7bbd6897
noviceGuru <amin.arabi@gmail.com>
2024-04-11 10:04:05 -0400

    another dataset added

--- solution.js
+++ solution.js
@@ -1,4 +1,4 @@
-const dataExample = {
+const exampleOne = {
     "idina_response": {
         "sources": [
             {
@@ -35,14 +35,37 @@ const dataExample = {
     }
 }
 
-const getDuplicates = () => {
+const exampleTwo = {
+    "idina_response":{
+      "sources":[
+        {
+          "url":"example.com",
+          "spam_score": 1,
+          "matching_target_indexes":[
+            0
+          ],
+          "matching_source_urls":[
+            {
+              "url":"example.com/news/current/",
+              "page_authority":44
+            }
+          ],
+          "domain_authority":95
+        },
+        // ...
+      ]
+    }
+  }
+
+
+const getDuplicates = (data) => {
     const spamScoresMap = new Map()
     const domainAuthoritiesMap = new Map()
 
     const dupSpamScores = new Set()
     const dupAuthorities = new Set()
 
-    dataExample.idina_response.sources.forEach(el => {
+    data.idina_response.sources.forEach(el => {
         if (spamScoresMap.has(el.spam_score)) {
             dupSpamScores.add(el.url)
             dupSpamScores.add(spamScoresMap.get(el.spam_score))
@@ -67,4 +90,5 @@ const getDuplicates = () => {
     return duplicates
 }
 
-console.log(getDuplicates(dataExample))
+console.log('Example One: ',getDuplicates(exampleOne))
+console.log('Example Two: ',getDuplicates(exampleTwo))
